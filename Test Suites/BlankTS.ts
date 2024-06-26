6fa1bcd2bb4b81669d6ea8941fcc806ac4dce77f
USER <idsta.matius@xl.co.id>
2024-06-26 18:46:32 +0700

    Added Blank Test Suites

---
+++ Test Suites/BlankTS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BlankTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6cbeab1c-2597-42eb-bce2-66262c194a97</testSuiteGuid>
+</TestSuiteEntity>
